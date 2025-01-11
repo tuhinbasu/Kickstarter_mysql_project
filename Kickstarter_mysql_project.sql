6030eead2b663b9115414a0ba829158d61f03ee0
tuhinbasu <89310102+tuhinbasu@users.noreply.github.com>
2025-01-11 22:34:46 +0530

    Update Kickstarter_mysql_project.sql

--- Kickstarter_mysql_project.sql
+++ Kickstarter_mysql_project.sql
@@ -1,17 +1,32 @@
--- Project Details --
+-- Describe the table --
 describe ksproject;
-
--- check duplicate --
+/* The DESCRIBE command offers detailed information about each column in a table, including the column name, data type, 
+whether it allows NULL values, indexing status, and default value.
+*/
+-- Drop the not required columns --
+alter table ksproject
+drop column goal,
+drop pledged,
+drop usd_pledged;
+/*
+Dropping 3 columns from this table which is redundant and will not be used for analysis-
+1. goal: The funding goal set by the project creator (in the project’s respective currency).
+2. pledged: The total amount of money backers pledged at the campaign's end. (country's respective currency)
+3. usd_pledged: Kickstar’s internal currency conversion system (exchange rate at the time of the campaign’s deadline) for 
+internal reporting and may not be as accurate or up-to-date as external currency.
+*/
+-- Refresh -- 
+-- Check duplicate rows (if present) --
 with check_duplicate as(
 select *,
-row_number()over(partition by id) as dup_rwn
+row_number()over(partition by id, name, category, main_category, currency, deadline, launched, state, backers, country, usd_pledged_real, usd_goal_real) as dup_rwn
 from ksproject)
 
 select * from check_duplicate
 where dup_rwn > 1;
--- no duplicate by id number --
+-- no duplicate found --
 
--- Find successfull, failed, & cancelled projects --
+-- Which are the high and low-performing categories?
 -- Successful --
 with success_projects as(
 select id, main_category,
@@ -22,8 +37,7 @@ select main_category,
 round(sum(success_parity)*100/count(success_parity),2) as success_rate
 from success_projects
 group by main_category
-order by success_rate desc
-limit 5;
+order by success_rate desc;
 
 -- Failed --
 with failed_projects as(
@@ -35,8 +49,7 @@ select main_category,
 round(sum(failed_parity)*100/count(failed_parity),2) as failed_rate
 from failed_projects
 group by main_category
-order by failed_rate desc
-limit 5;
+order by failed_rate desc;
 
 -- Canceled --
 with canceled_projects as(
@@ -48,10 +61,9 @@ select main_category,
 round(sum(canceled_parity)*100/count(canceled_parity),2) as canceled_rate
 from canceled_projects
 group by main_category
-order by canceled_rate desc
-limit 5;
+order by canceled_rate desc;
 
--- Which category within the main category with the highest pledged amount, and most backers
+--  Which categories have significant pledged amounts and a high number of backers?
 select main_category, category,
 round(sum(usd_pledged_real),2) as project_pledged_amount,
 sum(backers) as project_backers
@@ -59,6 +71,14 @@ from ksproject
 group by main_category, category
 order by project_pledged_amount desc
 limit 10;
+
+
+-- Backer Behaviour --
+select main_category,
+round(sum(usd_pledged_real)/sum(backers),2) as pledged_by_backer_ratio
+from ksproject
+group by main_category
+order by pledged_by_backer_ratio desc;
 
 -- Which country hosts the most successful projects
 with success_projects as(
@@ -117,7 +137,8 @@ from optimal_duration
 group by main_category
 order by project_duration desc;
 
--- Products that has performed good and low over the years --
+-- How can analyzing category trends over the years, based on success, failure, and cancellation rates, 
+-- help identify top-performing categories and those with untapped potential?
 with project_year as(
 select year(date(launched)) as year, main_category, category,
 sum(case state when "successful" then 1 else 0 end) as success_parity,
@@ -126,7 +147,9 @@ sum(case state when "cancelled" then 1 else 0 end) as cancelled_parity
 from ksproject
 group by year(date(launched)),main_category, category),
 
-project_performance as(
+-- Insert the below code for --
+-- Successful project performance over the years --
+project_success_performance as(
 select year, main_category, category,
 row_number()over(partition by year order by success_parity desc) as rwn
 from project_year)
@@ -135,12 +158,28 @@ select year, main_category, category
 from project_performance
 where rwn = 1;
 
--- Backer Behaviour --
-select main_category,
-round(sum(usd_pledged_real)/sum(backers),2) as pledged_by_backer_ratio
-from ksproject
-group by main_category
-order by pledged_by_backer_ratio desc;
+-- Insert the below code for --
+-- Failed project performance over the years--
+Failed
+project_failed_performance as(
+select year, main_category, category,
+row_number()over(partition by year order by failed_parity desc) as rwn
+from project_year)
+
+select year, main_category, category
+from project_failed_performance
+where rwn = 1;
+
+-- Insert the below code for --
+-- Cancelled project performance over the years --
+project_cancelled_performance as(
+select year, main_category, category,
+row_number()over(partition by year order by cancelled_parity desc) as rwn
+from project_year)
+
+select year, main_category, category
+from project_cancelled_performance
+where rwn = 1;
 
 
 
